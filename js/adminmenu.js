2517e5cd19fc7b4bb35c174b0435ed56af022b66
Fausto <faustocss10@gmail.com>
2024-09-01 23:52:25 -0300

    estoy hasta la pija

---
+++ js/adminmenu.js
@@ -0,0 +1,89 @@
+// barra de busqueda
+
+let dropdownBtn = document.getElementById('drop-text');
+let list = document.getElementById('list');
+let icon= document.getElementById('icon');
+let span = document.getElementById('span');
+let input = document.getElementById('search-input'); 
+
+let listItems = document.querySelectorAll('.dropdown-list-item');
+
+
+// busqueda
+document.querySelectorAll('.dropdown-list-item').forEach(item => {
+    item.addEventListener('click', function() {
+        document.getElementById('selected-category').value = this.textContent;
+        document.getElementById('span').textContent = this.textContent;
+    });
+});
+// fin busqueda
+
+
+
+
+
+// Manejo de la selección del dropdown
+document.querySelectorAll('.dropdown-list-item').forEach(item => {
+    item.addEventListener('click', function() {
+        document.getElementById('span').innerText = this.innerText;
+        document.getElementById('selected-category').value = this.innerText;
+    });
+});
+
+// fin obligar que seleccione una categoria
+
+
+
+
+// mostrar dropdown al clickear
+
+dropdownBtn.onclick = function(){
+    
+    // rotar icono
+    if(list.classList.contains('show')){
+    
+        icon.style.rotate = '0deg';
+    }else{
+    icon.style.rotate = '-180deg';
+    }      
+    list.classList.toggle('show');
+};
+
+window.onclick = function(e) {
+    if (
+        e.target.id !== "drop-text" &&
+        e.target.id !== "span" &&
+         e.target.id !== "icon"
+    ) {
+        list.classList.remove('show');
+        icon.style.rotate = '0deg';
+    }
+
+}
+
+for (item of listItems){
+    item.onclick = function(e){
+        span.innerText = e.target.innerHTML;
+        if(e.target.innerText == "Todo"){
+            input.placeholder = "Buscar";
+        }else{
+            input.placeholder = "Buscar en " + e.target.innerText;
+        }
+
+        input.placeholder = "Buscar en " + e.target.innerText;
+    }
+ 
+}
+
+
+
+
+
+
+// fin barra de busqueda
+
+
+
+
+
+
--- php/admin.php
+++ php/admin.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    
     <title>Aromas</title>
 </head>
 <body>
@@ -90,102 +91,25 @@
 
 
     <section>
-        <div class="container">
-
-            <div class="menu">
-                <ul class="lista-menu">
-                    <li> 
-                        <div id="drop-text" class="dropdown-text">
-                            <span id="span">Stock</span>
-                        </div>
-                    </li>
-                    <li>
-                        <div id="drop-text" class="dropdown-text">
-                            <span id="span">Estadisticas</span>
-                        </div>
-                    </li>
-                    <li>
-                        <div id="drop-text" class="dropdown-text">
-                            <span id="span">Publicar</span>
-                        </div>
-                    </li>
-                    <li>
-                        <div id="drop-text" class="dropdown-text">
-                            <span id="span">Reponer</span>
-                        </div>
-                    </li>
+        <div class="container-admin">
+            <div class="container-lista-admin">
+                <ul class="menu">
+                    <li class="active"><a href="#">Estadisticas</a></li>
+                    <li class="active"><a href="#">Inventario</a></li>
+                    <li class="active"><a href="#">Publicar</a></li>
+                    <li class="active"><a href="#">Mensajes</a></li>
                 </ul>
             </div>
 
+
+
             <div class="actividad">
-                <div class="stock" id="stock">
-                    <div class="container-tabla">
-                        <table border="1" class="table-stock">
-                            <thead>
-                                <div class="botones-tabla-container">
-                                    <button value="Eliminar" >Eliminar</button>
-                                    <div class="dropdown">
-                                        <div id="drop-text" class="dropdown-text">
-                                            <span id="span">Categorías</span>
-                                            <i id="icon" class="ri-arrow-down-s-line"></i>
-                                        </div>
-                                        <ul id="list" class="dropdown-list">
-                                            <li class="dropdown-list-item">Fragancias</li>
-                                            <li class="dropdown-list-item">Maquillajes</li>
-                                            <li class="dropdown-list-item">Faciales</li>
-                                            <li class="dropdown-list-item">Capilares</li>
-                                            <li class="dropdown-list-item">Personales</li>
-                                            <li class="dropdown-list-item">Regalería</li>
-                                            <li class="dropdown-list-item">Hogar</li>
-                                            <li class="dropdown-list-item">Accesorios</li>
-                                        </ul>
-                                        
-                                    </div>
-                                    
-                                    <input type="hidden" id="selected-category" name="category" value="Todo">
-                        
-                                    <div class="search-box">
-                                        <input type="text" id="search-input" name="search" placeholder="Buscar">
-                                        <button type="submit"><i class="ri-search-line"></i></button>
-                                    </div>
-                                    <button value="Guardar">Guardar</button>
-                                
-                                </div>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nombre</th>
-                                    <th>Precio</th>
-                                    <th>Stock</th>
-                                    <th>Operaciones</th>
-
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>2</td>
-                                    <td>3</td>
-                                    <td>4</td>
-                                    <td>5</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <div class="stock" id=""estadisticas"">
-
-                </div>
-                <div class="faltantes">
-
-                </div>
-                <div class="agregar">
-                    
-                </div>
+       
             </div>
         </div>
     </section>
 
     
-
+    <script src="../js/adminmenu.js?v=<?php echo time(); ?>"></script>
 </body>
 </html>
